b801a92e978700c031253f6eb1a6afc0756cc3fc
Kevin Flanagan <kflan3000@gmail.com>
2019-05-18 21:30:34 -0700

    main logic

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+
+
+DROP DATABASE burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+
+(
+	id int NOT NULL
+	AUTO_INCREMENT,
+	name varchar
+	(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY
+	(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+USE burger_db;
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Grape and Walnut Burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Rice and Sushi Burger');
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Wisconsin Cheese Curd Burger', true);
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Minced Crab Burger', true);
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Veggie Burger with Beef', true);
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Cheese and Bacon');
